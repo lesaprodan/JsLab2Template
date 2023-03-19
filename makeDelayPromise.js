33c3a0250eced65de7c04da685b2d70c887f7d8e
lesaprodan <99794388+lesaprodan@users.noreply.github.com>
2023-03-19 22:50:02 +0200

    Update makeDelayPromise.js

--- makeDelayPromise.js
+++ makeDelayPromise.js
@@ -7,7 +7,12 @@
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
 function makeDelayPromise(value, delayInMs){
-    // put your code here
-}
-
-console.log(makeDelayPromise('test', 1000));
+   
+return new Promise((resolve) => {
+   setTimeout ( () => {
+   resolve (value);
+   }, delayInMs);
+   });
+   }
+   makeDelayPromise("test",1000).then ( (value) =>{console.log (value==="test");
+   });
