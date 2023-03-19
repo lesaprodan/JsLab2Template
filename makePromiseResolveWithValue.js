858b5ef7f65de710092dfcd7919ef900dc0f76bd
lesaprodan <99794388+lesaprodan@users.noreply.github.com>
2023-03-20 00:03:14 +0200

    Update makePromiseResolveWithValue.js

--- makePromiseResolveWithValue.js
+++ makePromiseResolveWithValue.js
@@ -7,6 +7,13 @@
  */
 function makePromiseResolveWith(value){
     // put your code here
+return new Promise((resolve, reject) => {
+         resolve (value);
+    
+    });
 }
-
-console.log(makePromiseResolveWith(5));
+    makePromiseResolveWith(1).then(result => console.log (result === 1));
+    makePromiseResolveWith (3) .then(result => console.log(result === 3));
+    makePromiseResolveWith (22).then(result => console. log(result === 22));
+    makePromiseResolveWith(15).then(result => console.log (result === 15));
+    makePromiseResolveWith(100).then(result => console.log (result === 100));
